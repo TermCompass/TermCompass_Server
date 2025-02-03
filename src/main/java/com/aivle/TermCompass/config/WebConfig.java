d3f1a2af0260b1d3e712f2d622a8e09f61689226
JReal2 <dlwjdfuf99@naver.com>
2025-02-03 15:37:23 +0900

    feat: JWT 관련 Configuration 수정

--- src/main/java/com/aivle/TermCompass/config/WebConfig.java
+++ src/main/java/com/aivle/TermCompass/config/WebConfig.java
@@ -1,16 +1,31 @@
 package com.aivle.TermCompass.config;
 
+import com.aivle.TermCompass.filter.JwtAuthenticationFilter;
+import com.aivle.TermCompass.service.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public WebConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://admin.localhost:3000")
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
     }
 }
