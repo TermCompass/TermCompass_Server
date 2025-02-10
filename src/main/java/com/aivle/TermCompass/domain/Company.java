4c49085063366a789422b7afda1efcbcfa59142e
Jeongryeol Lee <dlwjdfuf99@naver.com>
2025-02-10 19:06:00 +0900

    feat: 사이트 등급 연동 시작

---
+++ src/main/java/com/aivle/TermCompass/domain/Company.java
@@ -0,0 +1,33 @@
+package com.aivle.TermCompass.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Rank rank;
+
+    private String logo;
+
+    private String link;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TermList> termLists = new ArrayList<>();
+
+    public enum Rank {
+        A, B, C, D, E
+    }
+}
---
+++ src/main/java/com/aivle/TermCompass/repository/CompanyRepository.java
@@ -0,0 +1,12 @@
+package com.aivle.TermCompass.repository;
+
+import com.aivle.TermCompass.domain.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByLink(String name);
+}
