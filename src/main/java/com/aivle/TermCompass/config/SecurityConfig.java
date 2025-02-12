630a711585b5e371518381b13be55fa813537be1
JinK <kyj9447@gmail.com>
2025-02-12 13:31:35 +0900

    config 변경

--- src/main/java/com/aivle/TermCompass/config/SecurityConfig.java
+++ src/main/java/com/aivle/TermCompass/config/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -32,38 +34,46 @@ public class SecurityConfig {
     }
 
     @Bean
-SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration corsConfig = new CorsConfiguration();
+                corsConfig.setAllowedOrigins(Arrays.asList("https://" + hostname,"https://admin." + hostname, "http://localhost:3000"));  // 클라이언트의 도메인 추가
+                corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+                corsConfig.setAllowCredentials(true);  // 쿠키와 같은 인증 정보를 허용
+                return corsConfig;
+            }))
             .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                    .requestMatchers("/","/login", "/signup").permitAll()
-                    .requestMatchers("/change-password").authenticated()
-                    .anyRequest().permitAll())
+                .requestMatchers("/", "/login", "/signup").permitAll()
+                .requestMatchers("/change-password").authenticated()
+                .anyRequest().permitAll())
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
             .formLogin(AbstractHttpConfigurer::disable)
-            .logout((logout) ->
-                    logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                            .logoutSuccessUrl("/logout-success")
-                            .invalidateHttpSession(true)
-                            .clearAuthentication(true)
-                            .addLogoutHandler((request, response, authentication) -> {
-                                Cookie cookie = new Cookie("jwt", "");
-                                cookie.setPath("/");
-                                cookie.setMaxAge(0);
-                                response.addCookie(cookie);
-                            })
+            .logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .addLogoutHandler((request, response, authentication) -> {
+                    Cookie cookie = new Cookie("jwt", "");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                })
             );
 
-    return http.build();
-}
+        return http.build();
+    }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
